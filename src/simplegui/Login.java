941dcece389968f17abccce0dc7748f66d0a3555
evanwmart <evanwmart@gmail.com>
2023-10-31 20:37:57 -0700

    working

--- src/simplegui/Login.java
+++ src/simplegui/Login.java
@@ -1,5 +1,9 @@
 package simplegui;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+    
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -10,15 +14,27 @@ import javafx.stage.Stage;
 public class Login extends VBox {
     public Login(Stage primaryStage) {
 
+        setSpacing(10);
+        setStyle("-fx-background-image: url('/assets/login-background.png'); -fx-background-size: cover; -fx-padding: 10; -fx-alignment: center;");
+        
+        VBox container = new VBox();
+        container.getStyleClass().add("container");
+
         Label titleLabel = new Label("Login Page");
+        titleLabel.getStyleClass().add("title-label");
         
         TextField usernameField = new TextField();
         usernameField.setPromptText("Username");
+        usernameField.getStyleClass().add("username-field"); 
+        usernameField.setPrefColumnCount(10); // Set preferred column count to 10
         
         PasswordField passwordField = new PasswordField();
         passwordField.setPromptText("Password");
+        passwordField.getStyleClass().add("password-field");
+        passwordField.setPrefColumnCount(10); // Set preferred column count to 10
         
         Button loginButton = new Button("Login");
+        loginButton.getStyleClass().add("login-button"); 
         loginButton.setOnAction(e -> {
             String username = usernameField.getText();
             String password = passwordField.getText();
@@ -32,12 +48,33 @@ public class Login extends VBox {
             }
         });
 
-        getChildren().addAll(titleLabel, usernameField, passwordField, loginButton);
+        container.setMaxWidth(600);
+        container.getChildren().addAll(titleLabel, usernameField, passwordField, loginButton);
+
+        getChildren().addAll(container);
     }
     
     private boolean authenticate(String username, String password) {
-        // Replace this with your authentication logic (e.g., check against a database)
-        // For demonstration purposes, this example always returns true.
-        return true;
+
+        String csvFile = "src/data/users.csv";
+        String line;
+        String csvSplitBy = ",";
+        boolean auth = false;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+                String[] user = line.split(csvSplitBy);
+                if (user[0].equals(username) && user[1].equals(password)) {
+                    auth = true;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return auth;
     }
 }
+
--- src/simplegui/Main.java
+++ src/simplegui/Main.java
@@ -3,6 +3,7 @@ package simplegui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 
 public class Main extends Application {
@@ -17,13 +18,22 @@ public class Main extends Application {
         Login login = new Login(primaryStage);
         StackPane root = new StackPane(login);
 
-        Scene scene = new Scene(root, 300, 200);
+        Scene scene = new Scene(root, 1000, 800);
+        scene.getStylesheets().add("res/styles.css");
         primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true);
         primaryStage.show();
     }
 
     public static void showMainPage(Stage primaryStage) {
-        // Replace this with code to display the main application after successful login
-        // You can create a new JavaFX scene and set it here.
+        Label welcomeLabel = new Label("Welcome!");
+        StackPane root = new StackPane();
+
+        root.getChildren().add(welcomeLabel);
+
+        Scene scene = new Scene(root, 1000, 800);
+        primaryStage.setTitle("Main Page");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
